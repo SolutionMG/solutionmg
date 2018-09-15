946589082321dce2f53d825ef4334c87411b93a4
SolutionMG <43059541+SolutionMG@users.noreply.github.com>
2018-09-15 23:22:55 +0900

    2D_0914hw

---
+++ hw_0914/py_05_2017180007.py
@@ -0,0 +1,113 @@
+from pico2d import *
+open_canvas()
+os.chdir('D:/')
+os.listdir()
+['$RECYCLE.BIN', '2D게임프로그래밍', 'Agario', 'Autodesk 3ds Max 2017 SP3 (x64) - Full', 'Autodesk 3ds Max 2017 SP3 (x64) - Full.rar', 'C&C++', 'eula.1042.txt', 'Git', 'GitDesktop', 'GOMPlayer', 'install.exe', 'install.ini', 'install.res.1042.dll', 'Maple', 'Nexonplay', 'Program Files', 'ProgramData', 'Steams', 'STOVE', 'System Volume Information', 'Unetsystem', 'vcredist.bmp', 'VC_RED.cab', 'VC_RED.MSI', 'WebLauncher', '그림', '비쥬얼 2017', '새 폴더', '새 폴더 (2)', '오버워칭!!!', '컴그']
+os.chdir('2D게임프로그래밍')
+os.listdir()
+['2dgp-2018-2-744dbe5565df9ad3bea1fa9940b29aab4fc9d529', 'solutionmg']
+os.chdir('2dgp-2018-2-744dbe5565df9ad3bea1fa9940b29aab4fc9d529')
+os.listdir()
+['.gitignore', 'pr0911_boy', 'pr0911_tree', 'testfile']
+os.chdir('pr0911_boy')
+os.listdir()
+['character.png', 'character_grass.py', 'character_moves.py', 'grass.png']
+grass=load_image('grass.png')
+character=load_image('character.png')
+grass.draw_now(400,30)
+character.draw_now(400,90)
+x=0
+y=90
+frame=0
+count=0
+while(count<2):
+    if(count==0):
+        while(x<750):
+            clear_canvas()
+            grass.draw(400,30)
+            character.clip_draw(frame*100,0,100,100,x,y)
+            update_canvas()
+            frame=(frame+1)%8
+            x+=5
+            delay(0.05)
+            get_events()
+        while(y<550):
+            clear_canvas()
+            grass.draw(400,30)
+            character.clip_draw(frame*100,0,100,100,x,y)
+            update_canvas()
+            frame=(frame+1)%8
+            y+=5
+            delay(0.05)
+            get_events()
+        while(x>20):
+            clear_canvas()
+            grass.draw(400,30)
+            character.clip_draw(frame*100,0,100,100,x,y)
+            update_canvas()
+            frame=(frame+1)%8
+            x-=5
+            delay(0.05)
+            get_events()
+        while(y>90):
+            clear_canvas()
+            grass.draw(400,30)
+            character.clip_draw(frame*100,0,100,100,x,y)
+            update_canvas()
+            frame=(frame+1)%8
+            y-=5
+            delay(0.05)
+            get_events()
+        while(x<375):
+            clear_canvas()
+            grass.draw(400,30)
+            character.clip_draw(frame*100,0,100,100,x,y)
+            update_canvas()
+            frame=(frame+1)%8
+            x+=5
+            delay(0.05)
+            get_events()
+        count=1
+    if(count==1):
+        while(x<750 and y<300):
+            clear_canvas()
+            grass.draw(400,30)
+            character.clip_draw(frame*100,0,100,100,x,y)
+            update_canvas()
+            frame=(frame+1)%8
+            y+=8
+            x+=10
+            delay(0.05)
+            get_events()
+        while(x>375 and y<600):
+            clear_canvas()
+            grass.draw(400,30)
+            character.clip_draw(frame*100,0,100,100,x,y)
+            update_canvas()
+            frame=(frame+1)%8
+            y+=8
+            x-=10
+            delay(0.05)
+            get_events()
+        while(x>0 and y>300):
+            clear_canvas()
+            grass.draw(400,30)
+            character.clip_draw(frame*100,0,100,100,x,y)
+            update_canvas()
+            frame=(frame+1)%8
+            y-=8
+            x-=10
+            delay(0.05)
+            get_events()
+        while(x<375 and y>90):
+            clear_canvas()
+            grass.draw(400,30)
+            character.clip_draw(frame*100,0,100,100,x,y)
+            update_canvas()
+            frame=(frame+1)%8
+            y-=8
+            x+=10
+            delay(0.05)
+            get_events()            
+        count=0                 
+
